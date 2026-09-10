6e7f562585f8778a8d16132e9192e52133a48fb4
Gaby <gabrieldurette17@gmail.com>
2026-09-10 16:57:01 -0400

    update

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-const CACHE = 'app-appart-v1'
+const CACHE = 'app-appart-v2'
 const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.json']
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting()
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)))
 })
 
@@ -9,12 +10,20 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(
       keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    ))
+    )).then(() => self.clients.claim())
   )
 })
 
+// réseau en premier : les mises à jour du site s'affichent tout de suite au prochain chargement,
+// le cache sert juste de secours si jamais il n'y a pas de connexion
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    fetch(event.request)
+      .then(reponse => {
+        const copie = reponse.clone()
+        caches.open(CACHE).then(cache => cache.put(event.request, copie))
+        return reponse
+      })
+      .catch(() => caches.match(event.request))
   )
 })
